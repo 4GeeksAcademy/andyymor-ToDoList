0b26870f9f350d74e5479ce615e29a01bfc46359
andyymor <61852990+andyymor@users.noreply.github.com>
2023-12-11 00:04:28 +0000

    fetch api project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,9 +8,40 @@ const Home = () => {
 	const [ inputValue, setInputValue ] = useState("");
 	const [todos, setTodos] = useState([]);
 
-	
+	useEffect(()=> {
+		const getToDos = async () => {
+			// get the data from the api
+			const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/andyymor`);
+			// convert the data to json
+			const data = await response.json();
+			setTodos(data)
+		}
+		getToDos();
+	}, [])
+
+	useEffect(()=> {
+		const postToDos = async () => {
+			// get the data from the api
+			const response = await fetch(`https://playground.4geeks.com/apis/fake/todos/user/andyymor`, 
+			{
+				method: "PUT",
+				body:JSON.stringify(todos),
+				headers:{'Content-type': 'application/json'}
+			});
+			// convert the data to json
+			const data = await response.json();
+			console.log(data)
+		}
+		postToDos();
+	}, [todos])
 
 
+	function addToDo(event) {
+		if(event.key === "Enter"){
+			const newTask = {label:inputValue, done:false}
+			setTodos([...todos, newTask])
+			setInputValue("");} 
+	}
 	return (
 		<div className="container">
 			<h1>My Todos</h1>
@@ -21,18 +52,13 @@ const Home = () => {
 				type="text" 
 				onChange={(e) => setInputValue(e.target.value)}
 				value={inputValue}
-				onKeyPress={(e) => {
-					if (e.key === "Enter" ) {
-						console.log("this code is running")
-					 setTodos(todos.concat(inputValue));
-					 setInputValue("");
-				}}}
+				onKeyDown={(event) =>addToDo(event)}
 				placeholder="what do you need to do "/> 
 			</li>
 
 			{todos.map((item, index) => (
 				<li>
-					{item} <i 
+					{item.label} <i 
 					class="fa-solid fa-trash-can"
 					onClick={() =>
 						setTodos(
